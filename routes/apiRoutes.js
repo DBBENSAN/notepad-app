b19fa74691eb84a2fabdc01eea270e871d379363
DBENSAN <dftb97@gmail.com>
2023-03-20 10:18:22 -0700

    refactor routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,93 +1,35 @@
 const notes = require("express").Router();
 const { v4: uuidv4 } = require('uuid');
+const { readFromFile, readAndAppend, writeToFile, } = require('../utils/fsUtils');
 
-
-function noteList() {
-   return new Promise((resolve, reject) => {
-       let items = [];
-       fs.readFile(path.join(__dirname, "../db/db.json"), "utf-8", (err, data) => {
-           if (err) {
-               reject(err);
-           } else {
-               if (data) {
-                   items = JSON.parse(data);
-               }
-               resolve(items);
-           }
-       });
-   });
-}
-
-notes.get("/", (req, res) => {
-    fs.readFile(path.join(__dirname, "../db/db.json"), "utf-8", (err, data) => {
-        if (err) {
-            console.error(err);
-            res.status(502).json("Error reading notes.");
-        } else {
-            res.status(200).json(JSON.parse(data));
-        }
-    });
+notesRouter.get('/', (req, res) => {
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 });
 
-
-notes.post("/", (req, res) => {
+notesRouter.post('/', (req, res) => {
     const { title, text } = req.body;
-
     if (req.body) {
         const newNote = {
             title,
             text,
-            id: uuidv4()
+            id: uuidv4(),
         };
-
-        let notesArr = [];
-        const currentNotes = noteList()
-            .then((response) => {
-                if (response === "Error") {
-                    res.status(502).json('Error in saving note');
-                } else {
-                    notesArr = response;
-                    notesArr.push(newNote);
-                    fs.writeFile(path.join(__dirname, "../db/db.json"), JSON.stringify(notesArr), (err) => {
-                        if (err) {
-                            console.log(err);
-                            res.status(500).json("Error in saving note.");
-                        } else {
-                            res.status(201).json("Note added successfully.");
-                        }
-                    });
-                }
-            });
+        readAndAppend(newNote, './db/db.json');
+        res.json(`Note added successfully`);
+    } else {
+        res.error('Error in adding note');
     }
 });
 
-notes.delete("/:id", (req, res) => {
-    const id = req.params.id;
-    let notesArr = [];
-    const currentNotes = noteList()
-        .then((response) => {
-            if (response === "Error") {
-                res.status(502).json("Error retrieving notes");
-            } else {
-                notesArr = response;
-                let idx;
-                for (let i = 0; i < notesArr.length; i++) {
-                    if (notesArr[i].id === id) {
-                        idx = i;
-                        break;
-                    }
-                }
-                notesArr.splice(idx, 1);
-                fs.writeFile(path.join(__dirname, "../db/db.json"), JSON.stringify(notesArr), (err) => {
-                    if (err) {
-                        console.log(err);
-                        res.status(500).json("Error in deleting note.");
-                    } else {
-                        res.status(200).json("Note deleted successfully.");
-                    }
-                });
-            }
-        })
+notesRouter.delete('/:id', (req, res) => {
+    const noteId = req.params.id;
+    readFromFile('./db/db.json')
+    .then((data) => JSON.parse(data))
+    .then((json) => {
+      const result = json.filter((note) => note.id !== noteId);
+      writeToFile('./db/db.json', result);
+      res.json(`Item ${noteId} has been deleted`);
+    });
 });
 
 module.exports = notes;
